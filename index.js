b6f99458c4eb20ef3c35b19a073161772c61c49e
Lyssa Laudencia <LJurado@walmartlabs.com>
2016-03-29 13:38:18 -0700

    Dropbox clone

--- index.js
+++ index.js
@@ -1,106 +1,67 @@
-let path = require('path')
-let fs = require('fs')
+"use strict"
+
+require('./helper')
+
+let fs = require('fs').promise
 let express = require('express')
 let morgan = require('morgan')
-let nodeify = require('bluebird-nodeify')
-let mime = require('mime-types')
-let rimraf = require('rimraf')
-let mkdirp = require('mkdirp')
-let argv = require('yargs')
-  .default('dir', process.cwd())
-  .argv
+let trycatch = require('trycatch')
+let wrap = require('co-express')
+let bodyParser = require('simple-bodyparser')
+let path = require('path')
 
-require('songbird')
-
-const NODE_ENV = process.env.NODE_ENV
-const PORT = process.env.PORT || 8000
-const ROOT_DIR = path.resolve(argv.dir)
-
-let app = express()
-
-if (NODE_ENV === 'development') {
-  app.use(morgan('dev'))
+function* main() {
+	let app = express()
+// 	app.all('*', (req, res) => res.end('hello\n'))
+	let port = 8000
+   	app.listen(port)
+   	
+   	app.get('*', wrap(read))
+   	app.put('*', wrap(create))
+	app.post('*', bodyParser(), wrap(update))
+	app.delete('*', wrap(remove))
+   	
+	console.log(`LISTENING @ http://127.0.0.1:${port}`)
+	
 }
 
-app.listen(PORT, ()=> console.log(`Listening @ http://127.0.0.1:${PORT}`))
-
-
-app.get('*', setFileMeta, sendHeaders, (req, res) => {
-  if (res.body) {
-    res.json(res.body)
-    return
-  }
-
-  fs.createReadStream(req.filePath).pipe(res)
-})
-
-app.head('*', setFileMeta, sendHeaders, (req, res) => res.end())
-
-app.delete('*', setFileMeta, (req, res, next) => {
-  async ()=>{
-    if (!req.stat) return res.send(400, 'Invalid Path')
-
-    if (req.stat.isDirectory()) {
-      await rimraf.promise(req.filePath)
-    } else await fs.promise.unlink(req.filePath)
-    res.end()
-  }().catch(next)
-})
-
-app.put('*', setFileMeta, setDirDetails, (req, res, next) => {
-  async ()=>{
-    if (req.stat) return res.send(405, 'File exists')
-    await mkdirp.promise(req.dirPath)
-
-    if (!req.isDir) req.pipe(fs.createWriteStream(req.filePath))
-    res.end()
-  }().catch(next)
-})
-
-app.post('*', setFileMeta, setDirDetails, (req, res, next) => {
-  async ()=>{
-    if (!req.stat) return res.send(405, 'File does not exist')
-    if (req.isDir || req.stat.isDirectory()) return res.send(405, 'Path is a directory')
-
-    await fs.promise.truncate(req.filePath, 0)
-    req.pipe(fs.createWriteStream(req.filePath))
-    res.end()
-  }().catch(next)
-})
-
-
-function setDirDetails(req, res, next) {
-  let filePath = req.filePath
-  let endsWithSlash = filePath.charAt(filePath.length-1) === path.sep
-  let hasExt = path.extname(filePath) !== ''
-  req.isDir = endsWithSlash || !hasExt
-  req.dirPath = req.isDir ? filePath : path.dirname(filePath)
-  next()
+function* read(req, res) {
+	console.log('calling read()...')
+	let filePath = path.join(__dirname, 'files', req.url)
+	//console.log('filePath: '+filePath)
+	let data = yield fs.readFile(filePath)
+	//console.log('data: '+data)
+	res.end(data)
 }
 
-function setFileMeta(req, res, next) {
-  req.filePath = path.resolve(path.join(ROOT_DIR, req.url))
-  if (req.filePath.indexOf(ROOT_DIR) !== 0) {
-    res.send(400, 'Invalid path')
-    return
-  }
-  fs.promise.stat(req.filePath)
-    .then(stat => req.stat = stat, ()=> req.stat = null)
-    .nodeify(next)
+function* create(req, res) {
+	console.log('calling create()...')
+	
+	//console.log('body: '+req.body)
+	let filePath = path.join(__dirname, 'files', req.url)
+	
+	let data = yield fs.open(filePath, "wx")
+	
+	//let data = yield fs.writeFile(filePath, req.body)
+	res.end()
 }
 
-function sendHeaders(req, res, next) {
-  nodeify(async ()=> {
-    if (req.stat.isDirectory()) {
-      let files = await fs.promise.readdir(req.filePath)
-      res.body = JSON.stringify(files)
-      res.setHeader('Content-Length', res.body.length)
-      res.setHeader('Content-Type', 'application/json')
-      return
-    }
-
-    res.setHeader('Content-Length', req.stat.size)
-    let contentType = mime.contentType(path.extname(req.filePath))
-    res.setHeader('Content-Type', contentType)
-  }(), next)
+function* update(req, res) {
+	console.log('calling update()...')
+	
+	console.log('body: '+req.body)
+	let filePath = path.join(__dirname, 'files', req.url)
+	let data = yield fs.writeFile(filePath, req.body)
+	
+	res.end()
 }
+
+function* remove(req, res) {
+	console.log('calling remove()...')
+	
+	let filePath = path.join(__dirname, 'files', req.url)
+	let data = yield fs.unlink(filePath)
+	res.end()
+}
+
+module.exports = main
